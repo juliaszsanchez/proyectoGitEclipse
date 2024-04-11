83c94c04afbce566c0204d15f90a97801df91d8a
Julia <Julia@DESKTOP-CLU2FEE>
2024-04-11 23:47:52 +0200

    Cambios para conflicto rama MASTER

--- documento1.java
+++ documento1.java
@@ -1,1 +1,23 @@
 //INSTANTANEA 4
+//INSTANTANEA 4
+
+public class Mi_Clase_1
+{
+	   
+    public static void main(String args[])
+    {   System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+    public static void main(String args[])
+    {   System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+   
+        }
+    }
+}
